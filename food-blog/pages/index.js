c23ae2e9a9e0b33da8ed203260aff68219216d30
Milly Prashar <millyprashar@gmail.com>
2022-07-07 16:28:27 -0700

    Added Cuisine elements

--- food-blog/pages/index.js
+++ food-blog/pages/index.js
@@ -4,7 +4,7 @@ const Homepage = (props) => {
   
   
   return (
-    <div className="h-screen bg-amber-50">
+    <div className="background-color text-cyan-900 bg-amber-50 pb-20">
       <div className="justify-center text-center pt-20 text-3xl">
         MISS AND MISTER ANTON EGO
       </div>
@@ -17,13 +17,17 @@ const Homepage = (props) => {
         </div>
       </div>
       <div className="w-full flex items-center place-content-center">
-        <p className="p-2 h-20 border border-black w-1/2 text-sm">
+        <p className="p-2 h-20 text-center border border-black w-1/2 text-sm">
           {' '}
-          Description ...{' '}
+          "I don't like food. I love it. If I don't love it, I don't swallow." -Anton Ego
+          {' '}
         </p>
       </div>
       <Cuisine title={'American'} type={'american'} />
       <Cuisine title={'Mexican'} type={'mexican'} />
+      <Cuisine title={'Japanese'} type={'japanese'} />
+      <Cuisine title={'Bakeries'} type={'bakeries'} />
+      <Cuisine title={'South Eastern'} type={'southEast'}/>
     </div>
   );
 };
